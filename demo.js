ad7990cc7aa65593e247749d84300f38df9aeae2
freewind <nowindlee@gmail.com>
2018-09-08 23:21:30 +0800

    fix code

--- demo.js
+++ demo.js
@@ -7,8 +7,7 @@ simpleGit.clone('git@github.com:freewind-demos/javascript-simple-git-push-commit
         fs.appendFileSync('local-repo/demo-project/README.md', '\n- Modified at: ' + new Date())
 
         const git = require('simple-git')('./local-repo/demo-project')
-        git.status(x => console.log(x))
-            .add('README.md', () => console.log('----- git add . -----'))
+        git.add('.', () => console.log('----- git add . -----'))
             .commit('modified by simplegit', () => console.log('------ git commit ------'))
             .push('origin', 'master', () => console.log('-------- git push --------'))
     })
