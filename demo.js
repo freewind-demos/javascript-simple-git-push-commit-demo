0d39caf06a86fd0d27be1439f9d09ca2fce8c121
freewind <nowindlee@gmail.com>
2018-09-08 23:20:05 +0800

    fix code

--- demo.js
+++ demo.js
@@ -1,13 +1,15 @@
-const workingDirPath = './local-repo'
-const simpleGit = require('simple-git')(workingDirPath)
+const simpleGit = require('simple-git')('./local-repo')
 const fs = require('fs')
 
 console.log('---- clone -----')
 simpleGit.clone('git@github.com:freewind-demos/javascript-simple-git-push-commit-demo.git', 'demo-project', () => console.log('------ git clone --------'))
     .exec(() => {
         fs.appendFileSync('local-repo/demo-project/README.md', '\n- Modified at: ' + new Date())
+
+        const git = require('simple-git')('./local-repo/demo-project')
+        git.status(x => console.log(x))
+            .add('README.md', () => console.log('----- git add . -----'))
+            .commit('modified by simplegit', () => console.log('------ git commit ------'))
+            .push('origin', 'master', () => console.log('-------- git push --------'))
     })
-    .add('./*', () => console.log('----- git add . -----'))
-    .commit('modified by simplegit', () => console.log('------ git commit ------'))
-    .push('origin', 'master', () => console.log('-------- git push --------'))
-    .catch((err) => console.error(err))
+
