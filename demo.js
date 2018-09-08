951b46d09883c3b96325b5c3981b677b7dfaef1a
freewind <nowindlee@gmail.com>
2018-09-08 23:12:26 +0800

    catch

--- demo.js
+++ demo.js
@@ -4,10 +4,10 @@ const fs = require('fs')
 
 console.log('---- clone -----')
 simpleGit.clone('git@github.com:freewind-demos/javascript-simple-git-push-commit-demo.git', 'demo-project', () => console.log('------ git clone --------'))
-    .exec(function () {
-        console.log('--- modify ---')
-        fs.appendFileSync('README.md', '\n- Modified at: ' + new Date())
+    .exec(() => {
+        fs.appendFileSync('local-repo/demo-project/README.md', '\n- Modified at: ' + new Date())
     })
     .add('./*', () => console.log('----- git add . -----'))
     .commit('modified by simplegit', () => console.log('------ git commit ------'))
     .push('origin', 'master', () => console.log('-------- git push --------'))
+    .catch((err) => console.error(err))
